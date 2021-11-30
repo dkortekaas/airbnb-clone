477fbee0f69edeb2c7ea8b596c1e8c382ab770bc
Dennis Kortekaas <d.kortekaas@gmail.com>
2021-11-30 23:51:31 +0100

    Footer section fix

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 function Footer() {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+        <footer className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
             <div className="space-y-4 text-xs text-gray-800">
                 <h5 className="font-bold">Support</h5>
                 <p className="cursor-pointer">Help Centre</p>
@@ -34,7 +34,7 @@ function Footer() {
                 <p className="cursor-pointer">Investors</p>
                 <p className="cursor-pointer">Airbnb Luxe</p>
             </div>
-        </div>
+        </footer>
     )
 }
 
