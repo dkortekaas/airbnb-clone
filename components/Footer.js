481cc3b8c448ab1a7090c0c9af74abac38af251e
Dennis Kortekaas <d.kortekaas@gmail.com>
2021-11-30 23:26:58 +0100

    Footer links fix

--- components/Footer.js
+++ components/Footer.js
@@ -3,36 +3,36 @@ function Footer() {
         <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
             <div className="space-y-4 text-xs text-gray-800">
                 <h5 className="font-bold">Support</h5>
-                <p>Help Centre</p>
-                <p>Safety information</p>
-                <p>Cancellation options</p>
-                <p>Our COVID-19 Response</p>
-                <p>Supporting people with disabilities</p>
-                <p>Airbnb Luxe</p>
+                <p className="cursor-pointer">Help Centre</p>
+                <p className="cursor-pointer">Safety information</p>
+                <p className="cursor-pointer">Cancellation options</p>
+                <p className="cursor-pointer">Our COVID-19 Response</p>
+                <p className="cursor-pointer">Supporting people with disabilities</p>
+                <p className="cursor-pointer">Airbnb Luxe</p>
             </div>
             <div className="space-y-4 text-xs text-gray-800">
                 <h5 className="font-bold">Community</h5>
-                <p>Airbnb.org: disaster relief housing</p>
-                <p>Support Afghan refugees</p>
-                <p>Celebrating diversity & belonging</p>
-                <p>Combating discrimination</p>
+                <p className="cursor-pointer">Airbnb.org: disaster relief housing</p>
+                <p className="cursor-pointer">Support Afghan refugees</p>
+                <p className="cursor-pointer">Celebrating diversity & belonging</p>
+                <p className="cursor-pointer">Combating discrimination</p>
             </div>
             <div className="space-y-4 text-xs text-gray-800">
                 <h5 className="font-bold">Hosting</h5>
-                <p>Try hosting</p>
-                <p>AirCover: protection for Hosts</p>
-                <p>Explore hosting resources</p>
-                <p>Visit our community forum</p>
-                <p>How to host responsibly</p>
+                <p className="cursor-pointer">Try hosting</p>
+                <p className="cursor-pointer">AirCover: protection for Hosts</p>
+                <p className="cursor-pointer">Explore hosting resources</p>
+                <p className="cursor-pointer">Visit our community forum</p>
+                <p className="cursor-pointer">How to host responsibly</p>
             </div>
             <div className="space-y-4 text-xs text-gray-800">
                 <h5 className="font-bold">About</h5>
-                <p>Newsroom</p>
-                <p>Learn about new features</p>
-                <p>Letter from out founders</p>
-                <p>Careers</p>
-                <p>Investors</p>
-                <p>Airbnb Luxe</p>
+                <p className="cursor-pointer">Newsroom</p>
+                <p className="cursor-pointer">Learn about new features</p>
+                <p className="cursor-pointer">Letter from out founders</p>
+                <p className="cursor-pointer">Careers</p>
+                <p className="cursor-pointer">Investors</p>
+                <p className="cursor-pointer">Airbnb Luxe</p>
             </div>
         </div>
     )
