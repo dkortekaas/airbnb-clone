8fac94a49b932f505fbec0585d00f3896cddbfba
Dennis Kortekaas <d.kortekaas@gmail.com>
2021-12-01 17:23:26 +0100

    Banner height fix

--- components/Banner.js
+++ components/Banner.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 function Banner() {
     return (
         <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]">
-            <Image src="https://a0.muscache.com/im/pictures/f1502649-e034-40ab-9fed-7992b7d550c6.jpg" layout="fill" />
+            <Image src="https://a0.muscache.com/im/pictures/f1502649-e034-40ab-9fed-7992b7d550c6.jpg" layout="fill" objectFit="cover" />
 
             <div className="absolute top-1/2 w-full text-center">
                 <p className="text-sm sm:text-5xl text-white font-semibold">Not sure where to go? Perfect.</p>
